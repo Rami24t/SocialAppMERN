eed442c669c50c335fb8f31390a939db8cb1f2d8
RamS24 <AlSaadi.Rami24@gmail.com>
2023-01-24 14:21:22 +0100

    install needed packages / add explanations

---
+++ explanation.js
@@ -0,0 +1,18 @@
+// Here is a brief explanation on each of the npm packages used in this project.
+// 1. express: This is a web framework for Node.js. It is used to create a server.
+// 2. body-parser: This is a middleware that is used to parse the body of the request.
+// 3. mongoose: This is a MongoDB object modeling tool designed to work in an asynchronous environment.
+// 4. bcrypt: This is a password hashing function.
+// 5. jsonwebtoken: This is a JSON Web Token implementation.
+// 6. Nodemailer: This is a module for Node.js applications to allow easy as cake email sending.
+// 7. cloudinary: This is a cloud service that offers a solution to a web application's entire image management pipeline.
+// 8. cookie-parser: This is a middleware that parses cookies attached to the client request object.
+// 9. multer: This is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files.
+// 10. nodemon: This is a utility that will monitor for any changes in your source and automatically restart your server.
+// 11. concurrently: This is a utility that will run multiple commands concurrently.
+// 12. axios: This is a promise based HTTP client for the browser and Node.js.
+// 13. react: This is a JavaScript library for building user interfaces.
+// 14. react-dom: This package serves as the entry point to the DOM and server renderers for React.
+// 15. react-router-dom: This is a collection of navigational components that compose declaratively with your application.
+// 16. react-scripts: This package includes scripts and configuration used by Create React App.
+// 17. multer-storage-cloudinary: This is a multer storage engine for storing files on cloudinary.
