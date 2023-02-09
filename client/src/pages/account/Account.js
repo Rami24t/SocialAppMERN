eff635b1aa47d124c4b8cb86b7d8d311e0793673
RamS24 <AlSaadi.Rami24@gmail.com>
2023-02-09 14:17:11 +0100

    updt acc pg

--- client/src/pages/account/Account.js
+++ client/src/pages/account/Account.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Account = () => {
+  const navigate = useNavigate()
+  navigate('/profile')
+
   return (<div>
-    Account Settings</div>
+    Account Settings page is not yet available in this demo version</div>
   )
 }
 
