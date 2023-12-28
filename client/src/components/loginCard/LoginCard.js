0ec4dc44558f124ffcd71266faf249b5e670deef
Rami <alsaadi.rami24@gmail.com>
2023-12-28 01:02:10 +0100

    FrontEnd - implement LogIn using GitHub (on vercel domain) / Login

--- client/src/components/loginCard/LoginCard.js
+++ client/src/components/loginCard/LoginCard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import {
   MDBBtn,
   MDBContainer,
@@ -11,6 +11,7 @@ import {
   MDBValidationItem,
   MDBInput,
   MDBCheckbox,
+  MDBIcon,
 } from "mdb-react-ui-kit";
 import { Link, useNavigate } from "react-router-dom";
 import "./LoginCard.css";
@@ -63,6 +64,40 @@ function LoginCard() {
       }
     }
   };
+
+  useEffect(() => {
+    const paramsCode = new URLSearchParams(window.location.search).get("code");
+    if (!paramsCode) return;
+    const handleGHLogin = async () => {
+      try {
+        const response = await axios.post(
+          baseUrl + "/users/gh-login",
+          {
+            code: paramsCode,
+          },
+          { withCredentials: true }
+        );
+        if (response.data?.success) {
+          // console.log("logged in successfully ! response.data:", response.data);
+          dispatch({
+            type: "saveProfile",
+            payload: response.data.user,
+          });
+          navigate("/home");
+          return response.data.success;
+        } else {
+          console.log("log in failed ! response.data:", response.data);
+          }
+      } catch (error) {
+        console.log("error:", error);
+        if(error.response.request.status === 404) {
+          alert("Please register first");
+          navigate("/register");
+        }
+      }
+    };
+    handleGHLogin(paramsCode);
+  }, []);
 
   return (
     <MDBContainer className="my-5 login">
@@ -133,7 +168,32 @@ function LoginCard() {
                 <MDBBtn onClick={(e) => handleLogin(e)} className="mb-4 w-100">
                   Log in
                 </MDBBtn>
-                <p className="mt-1 text-center">
+                <div className="text-center my-2">
+                  <p>or log in in with:</p>
+                  {/* <MDBBtn
+                    tag="a"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="google" size="sm" />
+                  </MDBBtn> */}
+
+                  <MDBBtn
+                    tag="a"
+                    color="none"
+                    className="mx-3 p-1"
+                    style={{ color: "#1266f1" }}
+                    onClick={() => {
+                      window.location.assign(
+                        `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GH_CLIENT_ID}&redirect_uri=${window.location.origin}${window.location.pathname}`
+                      );
+                    }}
+                  >
+                    <MDBIcon fab icon="github" size="xl" />
+                  </MDBBtn>
+                </div>
+                <p className="mt-5 text-center">
                   New user? Sign up now for a new account
                 </p>
                 <Link to="/register">
